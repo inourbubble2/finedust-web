225f679dd2f904b6c9c04bd7fd1ba0be4b22fbe5
Nayeon Kim <inourbubble2@gmail.com>
2021-10-08 05:22:01 +0900

    feat: 서울열린데이터광장 미세먼지 api와 연동하여 구 별로 다르게 색상을 보여주게끔 한다

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -6,7 +6,8 @@
 
 <script>
 import * as d3 from 'd3'
-const MAP_GEOJSON = require('../../seoul.geo.json')
+import axios from 'axios';
+const MAP_GEO_JSON = require('../../seoul.geo.json')
 
 export default {
   name: "Map",
@@ -15,7 +16,7 @@ export default {
   },
   data() {
     return {
-
+      data: null
     }
   },
   computed: {
@@ -28,8 +29,31 @@ export default {
     this.drawMap();
   },
   methods: {
-    drawMap() {
-      const geojson = MAP_GEOJSON;
+    async getTodayFineDust() {
+      const API_KEY = process.env.VUE_APP_API_KEY;
+      const url = 'http://openAPI.seoul.go.kr:8088/' + API_KEY + '/json/ListAirQualityByDistrictService/1/30';
+
+      const data = await axios.get(url);
+      return data['data']['ListAirQualityByDistrictService']['row'];
+    },
+
+    async responseToData(response) {
+      let data = {};
+      for (let i = 0; i < response.length; i++) {
+        const city = response[i];
+        const gu = city['MSRSTENAME'];
+        const pm10 = city['PM10'];
+        const grade = city['GRADE'];
+        data[gu] = {'pm10':pm10, 'grade':grade};
+      }
+      return data;
+    },
+
+    async drawMap() {
+      const response = await this.getTodayFineDust();
+      const data = await this.responseToData(response);
+
+      const geo_json = MAP_GEO_JSON;
 
       // 현재 브라우저의 크기 계산
       const divWidth = document.getElementById("map-wrapper").clientWidth;
@@ -43,19 +67,11 @@ export default {
           .attr('width', width)
           .attr('height', height);
 
-      // 배경 그리기
-      // const background = svg.append('rect')
-      //     .attr('class', 'background')
-      //     .attr('width', width)
-      //     .attr('height', height);
-
       // 지도가 그려지는 그래픽 노드(g) 생성
       const g = svg.append('g');
-      // const effectLayer = g.append('g').classed('effect-layer', true);
+
       // 지도가 그려지는 그래픽 노드
       const mapLayer = g.append('g').classed('map-layer', true);
-      // // 아이콘이 그려지는 그래픽 노드
-      // const iconsLayer = g.append('g').classed('icons-layer', true);
 
       // 지도의 출력 방법을 선택(메로카토르)
       let projection = d3.geoMercator()
@@ -64,32 +80,44 @@ export default {
 
       // svg 그림의 크기에 따라 출력될 지도의 크기를 다시 계산
       const path = d3.geoPath().projection(projection);
-      const bounds = path.bounds(geojson);
+      const bounds = path.bounds(geo_json);
       const widthScale = (bounds[1][0] - bounds[0][0]) / width;
       const heightScale = (bounds[1][1] - bounds[0][1]) / height;
       const scale = 0.95 / Math.max(widthScale, heightScale);
-      const xoffset = width/2 - scale * (bounds[1][0] + bounds[0][0]) /2 + 0;
-      const yoffset = height/2 - scale * (bounds[1][1] + bounds[0][1])/2 + 0;
-      const offset = [xoffset, yoffset];
+      const x_offset = width / 2 - scale * (bounds[1][0] + bounds[0][0]) / 2 + 0;
+      const y_offset = height / 2 - scale * (bounds[1][1] + bounds[0][1]) / 2 + 0;
+      const offset = [x_offset, y_offset];
       projection.scale(scale).translate(offset);
 
-      const color = d3.scaleLinear()
-          .domain([1, 20])
-          .clamp(true)
-          .range(['#595959', '#595959']);
+      function pmToColor(pm) {
+        if (pm < 10) {
+          return '#6ba5e3';
+        } else if (pm < 20) {
+          return '#67e073';
+        } else if (pm < 30) {
+          return '#e3c764';
+        } else {
+          return '#e36464';
+        }
+      }
+
+      function nameToPm(d) {
+        return data[d.properties.name]['pm10'];
+      }
 
       // Get province color
-      function fillFn(){
-        return color(0);
+      function fillFn(d) {
+        const pm10 = nameToPm(d);
+        return pmToColor(pm10);
       }
 
       mapLayer
-          .selectAll('path')
-          .data(geojson.features)
-          .enter().append('path')
-          .attr('d', path)
-          .attr('vector-effect', 'non-scaling-stroke')
-          .style('fill', fillFn);
+        .selectAll('path')
+        .data(geo_json.features)
+        .enter().append('path')
+        .attr('d', path)
+        .attr('vector-effect', 'non-scaling-stroke')
+        .style('fill', fillFn);
     }
   }
 }
