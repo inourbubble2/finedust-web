739a0ce5717631fcc20528a1ccce7acf4d9f6e27
Nayeon Kim <inourbubble2@gmail.com>
2021-10-26 05:17:23 +0900

    feat: 그래프에 색상을 추가한다

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -10,10 +10,39 @@ import axios from "axios";
 
 export default {
   name: "Graph",
+  props: [
+    "type", "year", "month", "day", "hour"
+  ],
+  data() {
+    return {
+      is_first_drawing: true,
+      loading: true,
+    }
+  },
+  watch: {
+    year() {
+      this.drawGraph();
+    },
+    month() {
+      this.drawGraph();
+    },
+    day() {
+      this.drawGraph();
+    },
+    hour() {
+      this.drawGraph();
+    }
+  },
   mounted() {
     this.drawGraph();
   },
   methods: {
+    async getPastFineDust(year, month, day, hour) {
+      const url = 'http://localhost:5000/pm?' + 'year=' + year + '&month=' + month + '&day=' + day + '&hour=' + hour;
+      const data = await axios.get(url);
+      return data.data;
+    },
+
     async getTodayFineDust() {
       const API_KEY = process.env.VUE_APP_API_KEY;
       const url = 'http://openAPI.seoul.go.kr:8088/' + API_KEY + '/json/ListAirQualityByDistrictService/1/30';
@@ -33,21 +62,36 @@ export default {
       }
       return data;
     },
-
     async drawGraph() {
-      const response = await this.getTodayFineDust();
-      let data = await this.responseToData(response);
+      let data;
+      if (this.type == 'now') {
+        const response = await this.getTodayFineDust();
+        data = await this.responseToData(response);
+      } else {
+        const response = await this.getPastFineDust(this.year, this.month, this.day, this.hour);
+        data = response['pm_result'];
+      }
 
-
-      const divWidth = document.getElementById("graph-wrapper").clientWidth;
-      const width = (divWidth < 800) ? divWidth * 0.9 : divWidth;
+      // const divWidth = document.getElementById("graph-wrapper").clientWidth;
+      // const width = (divWidth < 800) ? divWidth * 0.9 : divWidth / 3;
+      const width = 700;
       const scaleFactor = 5;
-      const barHeight = 20;
+      const barHeight = 30;
+//here
+      let graph;
+      if (this.is_first_drawing) {
+        graph = d3.select("#graph-wrapper")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", barHeight * Object.values(data).length);
+        this.is_first_drawing = false;
+      } else {
+        graph = d3.select("#graph-wrapper")
+            .select("svg")
+            .attr("width", width)
+            .attr("height", barHeight * Object.values(data).length);
+      }
 
-      const graph = d3.select("#graph-wrapper")
-          .append("svg")
-          .attr("width", width)
-          .attr("height", barHeight * Object.values(data).length);
 
       const bar = graph.selectAll("g")
           .data(Object.values(data))
@@ -57,16 +101,33 @@ export default {
             return "translate(0," + i * barHeight + ")";
           });
 
+      function pmToColor(pm) {
+        if (pm <= 15) {
+          return '#6ba5e3';
+        } else if (pm <= 80) {
+          return '#67e073';
+        } else if (pm <= 150) {
+          return '#e3c764';
+        } else if (pm <= 300) {
+          return '#e36464';
+        } else {
+          return '#FFFFFF';
+        }
+      }
+
       bar.append("rect").attr("width", function (d) {
         return d * scaleFactor;
-      }).attr("height", barHeight - 3);
+      }).attr("height", barHeight - 3)
+          .attr("fill", function (d) {
+            console.log(d);
+            return pmToColor(d);
+          });
 
       bar.append("text")
-          .attr("x", function (d) {
-            return (d * scaleFactor);
-          })
+          .attr("x", 3)
           .attr("y", barHeight / 2)
           .attr("dy", ".35em")
+          .attr("fill", "#FFFFFF")
           .text(function (d, i) {
             return " " + Object.keys(data)[i] + " " + Object.values(data)[i];
           });
@@ -82,7 +143,8 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 10px;
+  padding: 30px;
+  background-color: white;
 
   & > #graph-wrapper {
     width: 100%;
